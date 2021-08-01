95db7a89b8da18307f6685e7786959376f1fcbba
Talha Yazar <talhayazarg@gmail.com>
2021-08-01 17:47:30 +0300

    First Commit of my practices

---
+++ CalebEDU1/CalebEDU1/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalebEDU1
+{
+    public class Person
+    {
+        public static int x = 1;
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string FullName  
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public string GetFullName()
+        //
+        // Summary:
+        //     Writes the text representation of the specified object, followed by the current
+        //     line terminator, to the standard output stream using the specified format information.
+        //
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
---
+++ CalebEDU1/CalebEDU1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CalebEDU1                         //a namespace - used for organization
+{
+    class Program                           //a class - has members (such as method)
+    {
+        static void Main(string[] args)     //a method - contains a block of code
+        {
+            Console.WriteLine(Person.x);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            int l = 0;
+            bool o = false;
+            while (true)
+            {
+                //Console.WriteLine("kac?");
+                //int l = Int32.Parse(Console.ReadLine());
+                
+                for (int u = l; u < 15; u+=2) Console.WriteLine("");
+
+                for (int i = 0; i <= l; i++)
+                {
+                    
+                    for (int j = 0; j <= l; j++)
+                    {
+                        if (i == j)
+                        {
+                            for (int u = l; u < 10; u++) Console.Write(" ");
+                            Console.Write("\\");
+                            for (int k = 0; k < (l - i); k++)
+                            {
+                                if (i == 0)
+                                {
+                                    Console.Write('\u203E');
+                                    Console.Write('\u203E');
+                                }
+                                else
+                                {
+                                    Console.Write("  ");
+                                }
+                            }
+                            Console.Write("/");
+                            Console.Write("\n");
+                            break;
+                        }
+                        Console.Write(" ");
+                    }
+                }
+                System.Threading.Thread.Sleep(100);
+                Console.Clear();
+                if (!o)
+                {
+                    l+=2;
+                    if (l == 12) o = true;
+                }
+                if (o)
+                {
+                    l-= 2;
+                    if (l == 0) o = false;
+                }
+            }
+        }
+    }
+}
+
+//namespace -> classes -> members -> method -> statements
