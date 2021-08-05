3526e7cb09b673723e6f4dda014d0f9773115585
Talha Yazar <talhayazarg@gmail.com>
2021-08-05 08:51:46 +0300

    Second Subject

---
+++ CalebEDU2/CalebEDU2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalebEDU2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 9; i >= 0; i--)
+            {
+                
+                for(int j = i; j >= 0; j--)
+                {
+                    if (j < 4) continue;
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
